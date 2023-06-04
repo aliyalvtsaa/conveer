63c996a6b0bd015de14df110ddc7cdd7a1f15eef
aliyalvtsaa <128593426+aliyalvtsaa@users.noreply.github.com>
2023-06-04 20:30:47 +0300

    Update a.py

--- a.py
+++ a.py
@@ -64,18 +64,6 @@ import joblib
 #загрузим сохраненную модель классификации
 pipeline = joblib.load('pipeline_file.pkl')
 
-import pandas as pd
-# создадим пустой датасет и добавим в него строку с нашими данными
-
-new_row = {'LOAN': LOAN,'VALUE': VALUE, 'MORTDUE': MORTDUE, 'REASON': REASON,
-           'JOB': JOB, 'YOJ': YOJ, 'DEROG': DEROG,
-           'DELINQ': DELINQ, 'CLAGE': CLAGE, 'NINQ': NINQ,
-           'CLNO': CLNO, 'DEBTINC': DEBTINC}
-new_row = pd.DataFrame(new_row, index=[0])
-df2=pd.read_csv('data.py')
-df2 = pd.concat([df2, new_row], ignore_index=True)
-df2.to_csv('data.py')
-####
 data_file = 'data.csv'
 if st.session_state.get('loaded_data'):
     df = pd.read_csv(data_file)
@@ -85,28 +73,22 @@ else:
     df = pd.DataFrame(columns=column_names)
     st.session_state['loaded_data'] = True
 
-# Add a new row to the DataFrame
-if st.button('Add Row'):
-    new_row = {'LOAN': 100000, 'MORTDUE': 200000, 'VALUE': 300000, 'REASON': 'Refinance',
-               'JOB': 'Engineer', 'YOJ': 5, 'DEROG': 0, 'DELINQ': 0, 'CLAGE': 150, 'NINQ': 1,
-               'CLNO': 20, 'DEBTINC': 30}
-    df = df.append(new_row, ignore_index=True)
-    df.to_csv(data_file, index=False)  # Save the DataFrame to CSV
-
-# Display the DataFrame
-st.dataframe(df)
-####
-
-
-y_pred_proba = pipeline.predict_proba(df2)
 image = Image.open('star.png').resize((270, 300))
 
 #импортируем функцию для связи с внешними данными
 from my_functions import otvet
 if st.button('Рассчитать'):
     with st.spinner('Пожалуйста, подождите...'):
+        new_row = {'LOAN': LOAN,'VALUE': VALUE, 'MORTDUE': MORTDUE, 'REASON': REASON,
+           'JOB': JOB, 'YOJ': YOJ, 'DEROG': DEROG,
+           'DELINQ': DELINQ, 'CLAGE': CLAGE, 'NINQ': NINQ,
+           'CLNO': CLNO, 'DEBTINC': DEBTINC}
+        new_row = pd.DataFrame(new_row, index=[0])
+        df = pd.concat([df, new_row], ignore_index=True)
+        y_pred_proba = pipeline.predict_proba(df[-1])
+        df.to_csv(data_file, index=False)  
         time.sleep(7)
-        st.write(df2)
+        st.write(df)
         if otvet(y_pred_proba)=='Вам предварительно одобрен кредит!':
             st.write('Вам предварительно одобрен кредит!')
             st.image(image)
