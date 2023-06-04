e14396f1d898431903d393d801f3e2061d4d9f90
aliyalvtsaa <128593426+aliyalvtsaa@users.noreply.github.com>
2023-06-04 19:21:51 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,2 +1,5 @@
 df2 = pd.DataFrame(columns=['LOAN', 'MORTDUE', 'VALUE', 'REASON', 'JOB', 'YOJ', 'DEROG',
                             'DELINQ', 'CLAGE', 'NINQ', 'CLNO', 'DEBTINC'])
+df2['REASON'] = df2['REASON'].replace(['На обустройство дома', 'Для консолидации долга'], [0, 1])
+df2['JOB'] = df2['JOB'].replace(['Другое', 'В продажах', 'Офисный работник', 'Менеджер', 'Профессор', 'Работаю на себя'], 
+                                [0, 1, 2, 3, 4, 5])
